78276c2547be04967a395d21523c20218f9c7259
Makbarooodev <makbarooo2009@gmail.com>
2026-09-26 07:40:04 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const projects = [
   "https://makbarooodev.page.gd/",
   "https://makbarooodev.page.gd/",
   "https://makbarooodev.page.gd/",
-  "https://makbarooodev.page.gd/"
+  "https://makbarooodev.netlify.app/"
 ];
 
 const imgs = document.querySelectorAll(".preview");

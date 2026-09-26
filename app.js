3f81aa4539334ec8a829b03a3c7b1657296f81d9
Makbarooodev <makbarooo2009@gmail.com>
2026-09-26 07:38:29 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const img = document.querySelector(".preview");
+const img = document.querySelectorAll(".preview");
 
 img.src =
   "https://api.webstractor.com/v1/screenshot?url=https%3A%2F%2Fmakbarooodev.page.gd%2F&width=1280&height=720&format=png&t=" +

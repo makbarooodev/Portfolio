5378dc6f890f7614a1d09e7f382683c0c4e74243
Makbarooodev <makbarooo2009@gmail.com>
2026-09-26 11:04:52 +0700

    update app.js

--- app.js
+++ app.js
@@ -14,5 +14,3 @@ imgs.forEach((img, index) => {
     encodeURIComponent(projects[index]) +
     "&width=1280&height=720&format=png";
 });
-
-https://api.webstractor.com/v1/screenshot?url=https://makbarooodev.page.gd&width=1280&height=720&format=png
